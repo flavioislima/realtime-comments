98a9d4b909ff9879ea083ca056e98a648977103e
Flavio Lima <flaviolima@Flavios-iMac.local>
2018-07-10 22:18:46 -0300

    Written a method to create the commentaries

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import 'bootstrap-css-only'
 import Comments from './components/Comments';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.postNewComment = this.postNewComment.bind(this)
+    this.state = {
+      comments: {}
+    }
+  }
+
+  postNewComment(comment) {
+    const comments = { ...this.state.comments }
+    const timestamp = Date.now()
+    comments[`comm-${timestamp}`] = comment
+
+    this.setState({ comments })
+  }
+
   render() {
     return (
       <div className="container">
-        <NewComment />
-        <Comments />
+        <NewComment postNewComment={this.postNewComment} />
+        <Comments comments={this.state.comments} />
       </div>
     );
   }
--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 
 export default class NewComment extends Component {
+    constructor(props) {
+        super(props)
+        this.handleEnter = this.handleEnter.bind(this)
+
+    }
+
+    handleEnter(event) {
+
+        if (event.keyCode === 13) {
+            this.props.postNewComment({
+                comment: this.refs.comment.value
+            })
+            this.refs.comment.value = ''
+        }
+    }
+
     render() {
         return (
             <div className="row">
-                <textarea placeholder="Write something!" className="form-control"></textarea>
-            </div>
+                <textarea
+                    ref="comment"
+                    onKeyDown={this.handleEnter}
+                    placeholder="Write something!"
+                    className="form-control">
+                </textarea>
+                <div style={{ marginTop: 10, marginLeft: 760 }}>
+                    <button className="btn" style={{ marginRight: 20 }}>Clear</button>
+                    <button className="btn btn-info" onClick={this.handleEnter}>Send</button>
+                </div>
+            </div >
         )
     }
 }
