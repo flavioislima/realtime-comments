06c7f46cc5aa49c9dec4fe7f1f6a438394f88a8b
Flavio Lima <flaviolima@Flavios-iMac.local>
2018-07-11 10:41:38 -0300

    Changed App to work with Firebase RealTime Database

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import NewComment from './components/NewComment'
+import base from './base'
 import 'bootstrap-css-only'
-import Comments from './components/Comments';
+import Comments from './components/Comments'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.postNewComment = this.postNewComment.bind(this)
     this.state = {
-      comments: {
-        '1': { comment: 'Example Comment 1' },
-        '2': { comment: 'Example Comment 2' },
-        '3': { comment: 'Example Comment 3' },
-      }
+      comments: {}
     }
+
+    this.refComments = base.syncState('comments', {
+      context: this,
+      state: 'comments'
+    })
   }
 
   postNewComment(comment) {
