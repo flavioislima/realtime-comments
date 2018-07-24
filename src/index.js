4d4e3d5fbb23e1986807fba8cbdea87de97d088f
Flavio Lima <flavioislima@gmail.com>
2018-07-24 10:56:09 -0300

    Updated file locations

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import base, { providers, auth } from './containers/base' //dependency injection
+import base, { providers, auth } from './base' //dependency injection
 
 ReactDOM.render(<App base={base} auth={auth} providers={providers} />, document.getElementById('root'));
 registerServiceWorker();
